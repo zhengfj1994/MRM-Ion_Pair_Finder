7fa3f3f35101b38901e4bdcde2ccfa5e681a9aca
zhengfj1994 <2472700387@qq.com>
2020-05-29 14:04:16 +0800

    20200529

--- R/MRM-Ion Pair Finder/example for MRM_Ion_Pair_Finder.R
+++ R/MRM-Ion Pair Finder/example for MRM_Ion_Pair_Finder.R
@@ -1,5 +1,10 @@
 # An example code to show how to use MRM_Ion_Pair_Finder.
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("xcms")
+BiocManager::install("CAMERA")
+
 source("D:\\github\\MRM-Ion_Pair_Finder\\R\\MRM-Ion Pair Finder\\MRM_Ion_Pair_Finder.R")
 
 data_ms1ms2_final <- MRM_Ion_Pair_Finder(file_MS1 = "D:\\github\\MRM-Ion_Pair_Finder\\Data\\MS1\\Delete Iso-Add Result.csv",
